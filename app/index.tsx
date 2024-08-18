5394bd1c03e05d6f018629f923efd45f6f2cf410
tazheeb-shamsi <thshamsi@hotmail.com>
2024-08-18 12:14:31 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Home = () => {
+  return (
+     <SafeAreaView>
+        <Text className='px-6'>Home!</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
