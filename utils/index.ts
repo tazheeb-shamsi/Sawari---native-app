b2ed85309e4b55813394bb74fc35706890f4afb5
tazheeb-shamsi <thshamsi@hotmail.com>
2024-08-19 19:26:08 +0530

    fix

--- utils/index.ts
+++ utils/index.ts
@@ -18,7 +18,7 @@ export const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
 export const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
   switch (variant) {
     case "primary":
-      return "text-white";
+      return "text-black";
     case "secondary":
       return "text-gray-100";
     case "danger":
